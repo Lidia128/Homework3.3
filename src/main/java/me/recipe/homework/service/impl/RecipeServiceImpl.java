9d4d7d84f6cf38dffcb9b81312ff7494734215e6
Лидия Дворкина <lideya832@mail.ru>
2023-02-03 22:12:25 +0300

    Скорректировала код, программа запустилась.

--- src/main/java/me/recipe/homework/service/impl/RecipeServiceImpl.java
+++ src/main/java/me/recipe/homework/service/impl/RecipeServiceImpl.java
@@ -17,116 +17,118 @@ import java.util.*;
 @SuppressWarnings("ALL")
 @Service
 public class RecipeServiceImpl implements RecipeService {
+    final private FilesRecipeService filesRecipeService;
+    private static Map<Integer, Recipe> recipes = new TreeMap<>();
+    private static int id = 0;
+    public RecipeServiceImpl(FilesRecipeService filesRecipeService) {
+        this.filesRecipeService = filesRecipeService;
+    }
 
-    private final FilesRecipeService filesRecipeService;
-
-
-
-    @PostConstruct
-    private void init() {
+        @PostConstruct
+        private void init () {
 //        readFromFile();
-        recipes = new TreeMap<>();
-    }
-
-    public Collection<Recipe> getAll() {
-        return recipes.values();
-    }
-
-    public Recipe addRecipe(Recipe recipe) {
-        if (recipes.containsValue(recipe)) {
-            throw new RuntimeException("Не может добавить рецепт, уже есть");
+            recipes = new TreeMap<>();
         }
-        recipes.put(++id, recipe);
-        return recipe;
-    }
 
-    @Override
-    public Recipe getRecipe(Integer id) {
-        Recipe recipe = recipes.get(id);
-        return recipe;
-    }
-
-    public Recipe getRecipeById(int id) {
-        if (recipes.containsKey(id)) {
-            return recipes.get(id);
-        } else {
-            throw new RuntimeException("Нет такого рецепта");
+        public Collection<Recipe> getAll () {
+            return recipes.values();
         }
-    }
 
-    public Recipe editRecipe(long id, Recipe recipe) {
-        if (recipes.containsKey(id)) {
-            recipes.put((int) id, recipe);
+        public Recipe addRecipe (Recipe recipe){
+            if (recipes.containsValue(recipe)) {
+                throw new RuntimeException("Не может добавить рецепт, уже есть");
+            }
+            recipes.put(++id, recipe);
+            return recipe;
+        }
+
+        @Override
+        public Recipe getRecipe (Integer id){
+            Recipe recipe = recipes.get(id);
+            return recipe;
+        }
+
+        public Recipe getRecipeById ( int id){
+            if (recipes.containsKey(id)) {
+                return recipes.get(id);
+            } else {
+                throw new RuntimeException("Нет такого рецепта");
+            }
+        }
+
+        public Recipe editRecipe ( long id, Recipe recipe){
+            if (recipes.containsKey(id)) {
+                recipes.put((int) id, recipe);
+                saveToFile();
+            }
+            return recipe;
+        }
+
+        @Override
+        public Recipe updateRecipe ( int id, Recipe recipe){
+            Recipe serviceRecipe = recipes.get(id);
+            serviceRecipe.setName(recipe.getName());
+            serviceRecipe.setTime(recipe.getTime());
+            serviceRecipe.setIngredients(recipe.getIngredients());
+            serviceRecipe.setSteps(recipe.getSteps());
             saveToFile();
+            return serviceRecipe;
         }
-        return recipe;
-    }
 
-    @Override
-    public Recipe updateRecipe(int id, Recipe recipe) {
-        Recipe serviceRecipe = recipes.get(id);
-        serviceRecipe.setName(recipe.getName());
-        serviceRecipe.setTime(recipe.getTime());
-        serviceRecipe.setIngredients(recipe.getIngredients());
-        serviceRecipe.setSteps(recipe.getSteps());
-        saveToFile();
-        return serviceRecipe;
-    }
-
-    @Override
-    public Recipe removeRecipe(int id) {
-        return null;
-    }
-
-    public boolean deleteRecipe(long id) {
-        if (recipes.containsKey(id)) {
-            recipes.remove(id);
-            return true;
+        @Override
+        public Recipe removeRecipe ( int id){
+            return null;
         }
-        return false;
-    }
 
-    public void deleteAllRecipe() {
-        recipes = new TreeMap<>();
-    }
-
-    @Override
-    public Path createRecipesFile() {
-        return null;
-    }
-
-    @Override
-    public File getDataFile() {
-        return null;
-    }
-
-    @Override
-    public boolean cleanDateFile() {
-        return false;
-    }
-
-    @Override
-    public byte[] exportTxt() {
-        return new byte[0];
-    }
-
-
-    private void saveToFile() {
-        try {
-            String json = new ObjectMapper().writeValueAsString(recipes);
-            filesRecipeService.saveToFile(json);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
+        public boolean deleteRecipe ( long id){
+            if (recipes.containsKey(id)) {
+                recipes.remove(id);
+                return true;
+            }
+            return false;
         }
-    }
 
-    private void readFromFile() {
-        String json = RecipeService.readFromFile();
-        try {
-            recipes = new ObjectMapper().readValue(json, new TypeReference<TreeMap<Integer, Recipe>>() {
-            });
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
+        public void deleteAllRecipe () {
+            recipes = new TreeMap<>();
+        }
+
+        @Override
+        public Path createRecipesFile () {
+            return null;
+        }
+
+        @Override
+        public File getDataFile () {
+            return null;
+        }
+
+        @Override
+        public boolean cleanDateFile () {
+            return false;
+        }
+
+        @Override
+        public byte[] exportTxt () {
+            return new byte[0];
+        }
+
+
+        private void saveToFile () {
+            try {
+                String json = new ObjectMapper().writeValueAsString(recipes);
+                filesRecipeService.saveToFile(json);
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        private void readFromFile () {
+            String json = RecipeService.readFromFile();
+            try {
+                recipes = new ObjectMapper().readValue(json, new TypeReference<TreeMap<Integer, Recipe>>() {
+                });
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
-}
